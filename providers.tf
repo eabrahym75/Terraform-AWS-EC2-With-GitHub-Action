8a4056b799ffe5b87d2f5e6ab9803b2d1c8672b1
eabrahym75 <ayantolaibraheemkehinde@gmail.com>
2023-04-24 17:33:21 +0100

    added github workflow for deployment to aws

--- providers.tf
+++ providers.tf
@@ -1,11 +1,8 @@
 terraform {
-  # backend "s3" {
-  #   bucket = "Lock-bucket-17891s"
-  #   dynamodb_table = "State-lock1789s"
-  #   key = "terraform.tfstate"
-  #   region = "us-east-1"
-  #   encrypt = true
-  # }
+  backend "s3" {
+    dynamodb_table = "State-lock1789s"
+    encrypt = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
